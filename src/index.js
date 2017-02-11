c34199d143ea0acac166794b9a07f7c77443e3f6
Alex <ivanov.alexey.92@gmail.com>
2017-02-11 15:01:08 +0300

    Refactoring

--- src/index.js
+++ src/index.js
@@ -1,4 +1,6 @@
 import _ from 'lodash';
+import fs from 'fs';
+import path from 'path';
 import parseObj from './parser';
 
 const gendiffObj = (before, after) => {
@@ -18,9 +20,16 @@ const gendiffObj = (before, after) => {
   return `{\n${lines}}`;
 };
 
+const getObj = (pathToFile) => {
+  const extname = path.extname(pathToFile);
+  const content = fs.readFileSync(pathToFile, 'utf8');
+
+  return parseObj(content, extname);
+};
+
 const gendiff = (path1, path2) => {
-  const obj1 = parseObj(path1);
-  const obj2 = parseObj(path2);
+  const obj1 = getObj(path1);
+  const obj2 = getObj(path2);
 
   if (obj1 && obj2) {
     return gendiffObj(obj1, obj2);
